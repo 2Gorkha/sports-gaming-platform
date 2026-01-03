cf1ca2ca85bbb08e2c0adab0ea21e8cbcb6b300a
unknown <Rahul>
2026-01-03 18:06:04 +0530

    Correct Cors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-
-
 import express from "express";
 import cors from "cors";
 import "./src/config/db.js";
-
 
 import authRoutes from "./src/routes/authRoutes.js";
 import gameRoutes from "./src/routes/gameRoutes.js";
 import favoriteRoutes from "./src/routes/favoriteRoutes.js";
 
 const app = express();
+
 app.use(cors({
-    origin: "*",
+    origin: [
+        "http://localhost:5173",
+        "https://sports-gaming-platform-frontend.vercel.app",
+        "*"
+    ],
     credentials: true
 }));
 
@@ -25,7 +27,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
